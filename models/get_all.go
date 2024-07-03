8882f1dbc35af8241f8466919cc52298b5cbbb8b
Igor Francesco <igorfrancesco2@gmail.com>
2024-07-03 14:17:14 -0300

    update method getAll

--- models/get_all.go
+++ models/get_all.go
@@ -2,7 +2,7 @@ package models
 
 import "github.com/igorfarodrigues/api-postgres/db"
 
-func GetAll(id int64) (todos []Todo, err error) {
+func GetAll() (todos []Todo, err error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
 		return
